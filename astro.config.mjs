f1719e3bc20b23988cc390a3129bfcc93dec53e2
lubdhak7414 <70806945+lubdhak7414@users.noreply.github.com>
2024-12-07 12:09:58 +0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lubdhak7414.github.io',
+  site: "https://saf1.me",
   integrations: [
     expressiveCode(expressiveCodeOptions),
     tailwind({
